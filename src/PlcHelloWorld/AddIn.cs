3b3ce6248251f70c368c03a172dccc7adb78794c
D064647 <carsten.loeher@sap.com>
2020-06-05 16:22:03 +0200

    No logic Add-In template

---
+++ src/PlcHelloWorld/AddIn.cs
@@ -0,0 +1,18 @@
+﻿using Sap.Plc.AddIn.AddInView;
+using System.AddIn;
+using System.AddIn.Pipeline;
+
+namespace PlcHelloWorld
+{
+    [AddIn("PlcHelloWorld", Description = "Hello World PLC Add-In", Publisher = "SAP Deutschland SE & Co KG", Version = "0.0.0.1")]
+    [QualificationData(AddInConstants.AddInGuid, "4950A53C-DAEC-4E55-A3D3-E7A2757DAB32")]
+
+    public partial class AddIn : AddInBase
+    {
+        public override bool Setup()
+        {
+            //--After successful initialization we return true
+            return true;
+        }
+    }
+}
